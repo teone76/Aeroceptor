85d080732353bc5262ebca0ec968802c917ba1fd
Ross86 <gianluca.rossetti@studio.unibo.it>
2015-03-04 16:52:49 +0100

    aggiornato data log

---
+++ followMeLed/fml.h
@@ -0,0 +1,159 @@
+/*
+*/
+
+// Mavlink interface
+#include "../GCS_MAVLink/include/mavlink/v1.0/common/mavlink.h"// Mavlink interface
+#include "../GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/ardupilotmega.h"
+#include "../GCS_MAVLink/include/mavlink/v1.0/ardupilotmega/mavlink.h"
+#include "../GCS_MAVLink/include/mavlink/v1.0/common/common.h"
+
+#ifndef __Fml_h
+#define __Fml_h
+
+
+#include "Arduino.h"
+#include <limits.h>
+
+
+
+struct FmlLocation
+{
+   friend class FmlDroneInterface;
+
+public:
+   bool isValid() const    { return valid; }
+   bool isUpdated() const  { return updated; }
+   uint32_t age() const    { return valid ? millis() - lastCommitTime : (uint32_t)ULONG_MAX; }
+   float lat();
+   float lng();
+   float relAlt();
+   FmlLocation() : valid(false), updated(false)
+   {}
+
+private:
+   bool valid, updated;
+   float latData, lngData, relAltData; 
+   uint32_t lastCommitTime;
+   
+   void commit();
+   void setLatitude(float latitude);
+   void setLongitude(float longitude);
+   void setRelativeAltitude(float relativeAltitude);
+};
+
+struct FmlOther
+{
+   friend class FmlDroneInterface;
+
+public:
+   bool isValid() const    { return valid; }
+   bool isUpdated() const  { return updated; }
+   uint32_t age() const    { return valid ? millis() - lastCommitTime : (uint32_t)ULONG_MAX; }
+   float gs();
+   float hdg();
+
+   FmlOther() : valid(false), updated(false)
+   {}
+
+private:
+   bool valid, updated;
+   float groundSpeedData, headingData; 
+   uint32_t lastCommitTime;
+   
+   void commit();
+   void setGroundSpeed(float groundspeed);
+   void setHeading(float heading);
+};
+
+
+class FmlDroneInterface
+{
+public:
+
+  FmlDroneInterface(char* name,int id, int type, int autopilot, int gcs_id, int serial_port);
+  void encode(char c); // process one character received from GPS
+ 
+ /* double distanceBetweenDrone(FmlDrone drone);
+  double courseTo(FmlDrone drone);*/
+
+  
+
+  char* getName();
+  int getId();
+  int getType();
+  int getAutopilot();
+  int getGcsId();
+  int getSerialPort();
+  bool isLocationUpdated();
+  void sendMavMsgHeartbeat();
+  void sendMissionItemNavWaypoint();
+  void getDataStream();
+  void setRifLatitude(float rifLatitude);
+  void setRifLongitude(float rifLongitude);
+  void setRifRelAltitude(float rifAltitude);
+  
+  float getLatitude();
+  float getLongitude();
+  float getRelativeAltitude();
+  float getGroundSpeed();
+  float getHeading();
+  
+private:
+  FmlLocation location;
+  FmlLocation rif_location;
+  FmlOther other;
+  char* name;
+  int id;
+  int type;
+  int autopilot;
+  int gcs_id;  
+  int serial_port;
+  mavlink_message_t msg;
+  mavlink_status_t status;
+  mavlink_heartbeat_t heartbeat;
+  mavlink_global_position_int_t global_position_int;
+  mavlink_vfr_hud_t vfr_hud; 
+  mavlink_mission_item_t mission_item; 
+  mavlink_request_data_stream_t request_data_stream;
+  void identifyMavMsg();
+};
+
+
+class FmlNav
+{
+public:
+
+  FmlNav(char* name);
+  char* getName();
+  
+private:
+
+  char* name;  
+};
+
+
+// Led
+class FmlLed
+{
+public:
+
+  FmlLed(char* name);
+  char* getName();
+  
+/* Choose a combination for your message:
+   first char is a capital char, meaning:
+   A: ....                          (4 flashes)
+   B: .........................     (25 flashes)
+   C: _________________________     (1 extended flash)
+   
+   
+   
+   1:red, 2:blue, 3:green, 4:white, 5:yellow, 6:purple, 7:azure
+  
+  void blinkStatus(char first, char second, char third, char fourth, char fifth); 
+private:
+
+  char* name;  
+};
+
+#endif // def(__FmlDrone_h)
--- followMeLed/followMeLed.ino
+++ followMeLed/followMeLed.ino
@@ -1,42 +1,28 @@
 
-
+#include <Wire.h>
 #include <TinyGPS.h>
 #include "fml.h"
 TinyGPS gps;
-
-#include <stdio.h>                     /* This ert_main.c example uses printf/fflush */
-#include "navigator.h"                 /* Model's header file */
-#include "rtwtypes.h"
-
 //class FmlDrone(char* name,int id, int type, int autopilot, int gcs_id, int serial_port)
 FmlDroneInterface rover("Emax",1,1,1,1,3);
 FmlDroneInterface multirotor_1("F550",2,2,2,10,1);
 FmlDroneInterface multirotor_2("F550",3,2,2,20,2);
-
 FmlNav navigator("ATmega2560");
-
-unsigned long mavTimer = millis();
-unsigned long mavInterval = 1000;
-
-unsigned long simulinkTimer = millis();
-unsigned long simulinkInterval = 100;
+FmlLed led("MyLed");
+unsigned long timer = millis();
+unsigned long interval = 1000;
+unsigned long pwmin = 0;
 
 
-
-
-/* Da cancellare
 float navigationLawInput [15];
 float navigationLawOutput [6];
-*/
 
 //pin input from RX
 int pin = 12;
-unsigned long pwmin = 0;
 
 void setup() {
-  /* Initialize model */
-  navigator_initialize();
-  
+
+  Wire.begin();          // I2C led
   Serial.begin(115200);  // Debug
   Serial1.begin(57600);  // Multicopter 1
   Serial2.begin(57600);  // Multicopter 2
@@ -59,54 +45,22 @@ void setup() {
   Serial.print("LNG_3    ");
   Serial.print("REL_ALT_3  ");
   Serial.print("GND_SPD_3    ");
-  Serial.print("HDG_3     ");
+  Serial.print("HDG_3      ");
   Serial.print("DIST   ");
-  Serial.print("CRS  ");
-  Serial.print("CALC_LAT    ");
-  Serial.print("CALC_LNG    ");
-  Serial.println("time");
+  Serial.println("CRS    ");
 }
 
 void loop() {  
-
   checkAvailableDataOnSerial(); 
   
-  // SIMULINK TASK - 10 HZ
-  if(millis() - simulinkTimer > simulinkInterval)
+  // 
+  if(millis() - timer > interval)
   { 
-  simulinkTimer = millis();
-
-  // Set model inputs  
-  navigator_U.In[0] = rover.getLatitude();
-  navigator_U.In[1] = rover.getLongitude();
-  navigator_U.In[2] = rover.getRelativeAltitude();
-  navigator_U.In[3] = rover.getGroundSpeed();
-  navigator_U.In[4] = rover.getHeading();
-  
-  navigator_U.In[5] = multirotor_1.getLatitude();
-  navigator_U.In[6] = multirotor_1.getLongitude();
-  navigator_U.In[7] = multirotor_1.getRelativeAltitude();
-  navigator_U.In[8] = multirotor_1.getGroundSpeed();
-  navigator_U.In[9] = multirotor_1.getHeading();  
-
-  navigator_U.In[10] = multirotor_2.getLatitude();
-  navigator_U.In[11] = multirotor_2.getLongitude();
-  navigator_U.In[12] = multirotor_2.getRelativeAltitude();
-  navigator_U.In[13] = multirotor_2.getGroundSpeed();
-  navigator_U.In[14] = multirotor_2.getHeading();
-  
-  // Navigation Control Law - Step the model 
-  navigator_step();
-  }
-  
-  // MAVLINK TASK - 1 HZ 
-  if(millis() - mavTimer > mavInterval)
-  { 
-    mavTimer = millis();  
+    timer = millis();
     // Invio del heatbeat a tutti i multirotori
     sendHeartbeat();  
     sendRequest();
-         
+       
    Serial.print(multirotor_1.getLatitude(), 7);
    Serial.print(", ");
    Serial.print(multirotor_1.getLongitude(), 7);
@@ -125,33 +79,26 @@ void loop() {
    Serial.print(", ");   
    Serial.print(gps.distance_between(rover.getLatitude(), rover.getLongitude(), multirotor_1.getLatitude(), multirotor_1.getLongitude()));
    Serial.print(", ");
-   Serial.print(gps.course_to(rover.getLatitude(), rover.getLongitude(), multirotor_1.getLatitude(), multirotor_1.getLongitude()));
-   Serial.print(", ");
-   Serial.print(navigator_Y.Out[0], 7);
-   Serial.print(", ");
-   Serial.print(navigator_Y.Out[1], 7);
-   Serial.print(", "); 
-   Serial.println(millis());   
+   Serial.println(gps.course_to(rover.getLatitude(), rover.getLongitude(), multirotor_1.getLatitude(), multirotor_1.getLongitude()));
   }  
 
      
   // 
   if(rover.isLocationUpdated())
   { 
-    // Aggiornamento dei reference point dei multirotore
-    updateDronePositionReference();
-    
+    // Calcolo della nuova posizione e settaggio della nuovo riferimento di posizione dei due multirotori
+    calculateNewDronePosition();
+
     // Command input from RX 
     pwmin = pulseIn(pin, HIGH, 20000);
-  
     //Serial.println(pwmin);
-    if(pwmin < 1200) {          
+    if(pwmin < 1200) {     
+    // 
       sendWaypoints();
-      //Serial.println("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+      Serial.println("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
     }         
-  }
 }
-
+}
 //////// FUNCTIONS /////////
 
 void checkAvailableDataOnSerial()
@@ -173,19 +120,44 @@ void sendHeartbeat()
   multirotor_2.sendMavMsgHeartbeat();   
 }
 
-
-void updateDronePositionReference()
+void calculateNewDronePosition()
 {
-  //OUTPUTS:
-  multirotor_1.setRifLatitude(navigator_Y.Out[0]);
-  multirotor_1.setRifLongitude(navigator_Y.Out[1]);
-  multirotor_1.setRifRelAltitude(navigator_Y.Out[2]);
+  // Set navigatio law INPUTS
+  navigationLawInput[0] = rover.getLatitude();
+  navigationLawInput[1] = rover.getLongitude();
+  navigationLawInput[2] = rover.getRelativeAltitude();
+  navigationLawInput[3] = rover.getGroundSpeed();
+  navigationLawInput[4] = rover.getHeading();
+  
+  navigationLawInput[5] = multirotor_1.getLatitude();
+  navigationLawInput[6] = multirotor_1.getLongitude();
+  navigationLawInput[7] = multirotor_1.getRelativeAltitude();
+  navigationLawInput[8] = multirotor_1.getGroundSpeed();
+  navigationLawInput[9] = multirotor_1.getHeading();  
 
-  multirotor_2.setRifLatitude(navigator_Y.Out[3]);
-  multirotor_2.setRifLongitude(navigator_Y.Out[4]);
-  multirotor_2.setRifRelAltitude(navigator_Y.Out[5]);
+  navigationLawInput[10] = multirotor_2.getLatitude();
+  navigationLawInput[11] = multirotor_2.getLongitude();
+  navigationLawInput[12] = multirotor_2.getRelativeAltitude();
+  navigationLawInput[13] = multirotor_2.getGroundSpeed();
+  navigationLawInput[14] = multirotor_2.getHeading();  
+
+ // Navigation Control Law
+ navigationLawOutput[0] =  navigationLawInput[0];
+ navigationLawOutput[1] =  navigationLawInput[1];
+ navigationLawOutput[2] =  navigationLawInput[7];
+ navigationLawOutput[3] =  navigationLawInput[0];
+ navigationLawOutput[4] =  navigationLawInput[1];
+ navigationLawOutput[5] =  navigationLawInput[12]; 
+  
+ //OUTPUTS:
+  multirotor_1.setRifLatitude(navigationLawOutput[0]);
+  multirotor_1.setRifLongitude(navigationLawOutput[1]);
+  multirotor_1.setRifRelAltitude(navigationLawOutput[2]);
+
+  /*multirotor_2.setRifLatitude(navigationLawOutput[3]); 
+  multirotor_2.setRifLongitude(navigationLawOutput[4]);
+  multirotor_2.setRifRelAltitude(navigationLawOutput[5]);*/
 }
-
 
 void sendWaypoints()
 {
